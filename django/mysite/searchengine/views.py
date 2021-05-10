d166ca81bf7b39ca0e67738d9f5ba11abe100d61
Jaya Ren <jayaren99@ucla.edu>
2021-05-09 21:52:01 -0700

    error in urls.py
    patterns not found

--- django/mysite/searchengine/views.py
+++ django/mysite/searchengine/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 
-# Create your views here.
+def search(request):
+
+    if request.POST:
+	print request.POST['term']
+	return HttpResponseRedirect("/")
+    else:
+	return render_to_response('search.html')
+
