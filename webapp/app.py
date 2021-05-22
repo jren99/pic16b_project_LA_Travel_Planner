b17585be16cd380f41193874303c7b9f597578cf
StancyZhang <jingxuanzhang@g.ucla.edu>
2021-05-22 20:31:17 +0800

    search box finished

--- webapp/app.py
+++ webapp/app.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, url_for, request, redirect
 #from datetime import datetime
 import pandas as pd
 import csv
+import requests
+from bs4 import BeautifulSoup as soup
 
 app = Flask(__name__)
 '''
@@ -60,27 +62,67 @@ dataframe_touristsite = dataframe_ts[["Rank", "Tourist Site Name", "Site Link"]]
 touristsite_header = tuple(dataframe_touristsite)
 touristsite_body = tuple(dataframe_touristsite.itertuples(index=False, name=None))
 
-@app.route('/touristsite/')
+@app.route('/touristsite/', methods=['POST', 'GET'])
 def touristsite():
-    return render_template('touristsite.html',headings=touristsite_header, data=touristsite_body)
+    if request.method == 'GET':
+            return render_template('touristsite.html',headings=touristsite_header, data=touristsite_body)
+    else:
+        try:
+            return render_template('touristsite.html', name=request.form['name'],headings=touristsite_header, data=touristsite_body)
+        except:
+            return render_template('touristsite.html',headings=touristsite_header, data=touristsite_body)
 
-dataframe_ht = pd.read_csv('450_hotel.csv')
+@app.route('/touristsite/<name>/', methods=['POST', 'GET'])
+def touristsite_name(name):
+    touristsite_header = tuple(dataframe_touristsite)
+    dataframe_touristsite_update = dataframe_touristsite[dataframe_touristsite["Tourist Site Name"].str.contains(str(name))]
+    touristsite_body_update = tuple(dataframe_touristsite_update.itertuples(index=False, name=None))
+    return render_template('touristsite.html', name=name, headings_update=touristsite_header, data_update=touristsite_body_update)
+
+dataframe_ht = pd.read_csv('419_hotel.csv')
 dataframe_hotel = dataframe_ht[["Rank", "Hotel Name", "Rate", "Site Link"]]
 hotel_header = tuple(dataframe_hotel)
 hotel_body = tuple(dataframe_hotel.itertuples(index=False, name=None))
 
-@app.route('/hotel/')
+@app.route('/hotel/', methods=['POST', 'GET'])
 def hotel():
-    return render_template('hotel.html',headings=hotel_header, data=hotel_body)
+    if request.method == 'GET':
+            return render_template('hotel.html',headings=hotel_header, data=hotel_body)
+    else:
+        try:
+            return render_template('hotel.html', name=request.form['name'],headings=hotel_header, data=hotel_body)
+        except:
+            return render_template('hotel.html',headings=hotel_header, data=hotel_body)
+
+@app.route('/hotel/<name>/', methods=['POST', 'GET'])
+def hotel_name(name):
+    hotel_header = tuple(dataframe_hotel)
+    dataframe_hotel_update = dataframe_hotel[dataframe_hotel["Hotel Name"].str.contains(str(name))]
+    hotel_body_update = tuple(dataframe_hotel_update.itertuples(index=False, name=None))
+    return render_template('hotel.html', name=name, headings_update=hotel_header, data_update=hotel_body_update)
 
 dataframe_rt = pd.read_csv('13460_restaurant.csv')
 dataframe_restaurant = dataframe_rt[["Rank", "Restaurant Name", "Style", "Rate", "Site Link"]]
 restaurant_header = tuple(dataframe_restaurant)
 restaurant_body = tuple(dataframe_restaurant.itertuples(index=False, name=None))
 
-@app.route('/restaurant/')
+@app.route('/restaurant/', methods=['POST', 'GET'])
 def restaurant():
-    return render_template('restaurant.html',headings=restaurant_header, data=restaurant_body)
+    if request.method == 'GET':
+            return render_template('restaurant.html',headings=restaurant_header, data=restaurant_body)
+    else:
+        try:
+            return render_template('restaurant.html', name=request.form['name'],headings=restaurant_header, data=restaurant_body)
+        except:
+            return render_template('restaurant.html',headings=restaurant_header, data=restaurant_body)
+
+@app.route('/restaurant/<name>/', methods=['POST', 'GET'])
+def restaurant_name(name):
+    dataframe_restaurant_drop = dataframe_restaurant.dropna()
+    restaurant_header = tuple(dataframe_restaurant_drop)
+    dataframe_restaurant_update = dataframe_restaurant_drop[dataframe_restaurant_drop["Style"].str.contains(str(name))]
+    restaurant_body_update = tuple(dataframe_restaurant_update.itertuples(index=False, name=None))
+    return render_template('restaurant.html', name=name, headings_update=restaurant_header, data_update=restaurant_body_update)
 
 '''
 @app.route('/delete/<int:id>')
